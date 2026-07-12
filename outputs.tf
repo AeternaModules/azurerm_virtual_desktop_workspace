1596fca6ace66f38e58ac46ef0d7f5532430213d
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:21:19 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,33 +1,29 @@
-output "virtual_desktop_workspaces" {
-  description = "All virtual_desktop_workspace resources"
-  value       = azurerm_virtual_desktop_workspace.virtual_desktop_workspaces
-}
 output "virtual_desktop_workspaces_description" {
-  description = "List of description values across all virtual_desktop_workspaces"
-  value       = [for k, v in azurerm_virtual_desktop_workspace.virtual_desktop_workspaces : v.description]
+  description = "Map of description values across all virtual_desktop_workspaces, keyed the same as var.virtual_desktop_workspaces"
+  value       = { for k, v in azurerm_virtual_desktop_workspace.virtual_desktop_workspaces : k => v.description }
 }
 output "virtual_desktop_workspaces_friendly_name" {
-  description = "List of friendly_name values across all virtual_desktop_workspaces"
-  value       = [for k, v in azurerm_virtual_desktop_workspace.virtual_desktop_workspaces : v.friendly_name]
+  description = "Map of friendly_name values across all virtual_desktop_workspaces, keyed the same as var.virtual_desktop_workspaces"
+  value       = { for k, v in azurerm_virtual_desktop_workspace.virtual_desktop_workspaces : k => v.friendly_name }
 }
 output "virtual_desktop_workspaces_location" {
-  description = "List of location values across all virtual_desktop_workspaces"
-  value       = [for k, v in azurerm_virtual_desktop_workspace.virtual_desktop_workspaces : v.location]
+  description = "Map of location values across all virtual_desktop_workspaces, keyed the same as var.virtual_desktop_workspaces"
+  value       = { for k, v in azurerm_virtual_desktop_workspace.virtual_desktop_workspaces : k => v.location }
 }
 output "virtual_desktop_workspaces_name" {
-  description = "List of name values across all virtual_desktop_workspaces"
-  value       = [for k, v in azurerm_virtual_desktop_workspace.virtual_desktop_workspaces : v.name]
+  description = "Map of name values across all virtual_desktop_workspaces, keyed the same as var.virtual_desktop_workspaces"
+  value       = { for k, v in azurerm_virtual_desktop_workspace.virtual_desktop_workspaces : k => v.name }
 }
 output "virtual_desktop_workspaces_public_network_access_enabled" {
-  description = "List of public_network_access_enabled values across all virtual_desktop_workspaces"
-  value       = [for k, v in azurerm_virtual_desktop_workspace.virtual_desktop_workspaces : v.public_network_access_enabled]
+  description = "Map of public_network_access_enabled values across all virtual_desktop_workspaces, keyed the same as var.virtual_desktop_workspaces"
+  value       = { for k, v in azurerm_virtual_desktop_workspace.virtual_desktop_workspaces : k => v.public_network_access_enabled }
 }
 output "virtual_desktop_workspaces_resource_group_name" {
-  description = "List of resource_group_name values across all virtual_desktop_workspaces"
-  value       = [for k, v in azurerm_virtual_desktop_workspace.virtual_desktop_workspaces : v.resource_group_name]
+  description = "Map of resource_group_name values across all virtual_desktop_workspaces, keyed the same as var.virtual_desktop_workspaces"
+  value       = { for k, v in azurerm_virtual_desktop_workspace.virtual_desktop_workspaces : k => v.resource_group_name }
 }
 output "virtual_desktop_workspaces_tags" {
-  description = "List of tags values across all virtual_desktop_workspaces"
-  value       = [for k, v in azurerm_virtual_desktop_workspace.virtual_desktop_workspaces : v.tags]
+  description = "Map of tags values across all virtual_desktop_workspaces, keyed the same as var.virtual_desktop_workspaces"
+  value       = { for k, v in azurerm_virtual_desktop_workspace.virtual_desktop_workspaces : k => v.tags }
 }
 
